3b0fe0c62594f620c522527d90bcd332ae3a2d49
msuleman2001 <msuleman2001@gmail.com>
2024-08-28 13:53:34 +0500

    added template

---
+++ resources/views/sbidu/sbidu-html/sbidu/contact.php
@@ -0,0 +1,24 @@
+﻿<?php
+$name = $_POST['name']; 
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+
+$to = 'demo@gmail.com';
+
+$from = 'rock@Sbidu.com';
+
+if($name && $email && $message){
+		$headers = "From:  $from \r\n";
+		$headers .= "Reply-To:  $name<$email> \r\n";
+		$headers .= "MIME-Version: 1.0\r\n";
+		$headers .= "Content-Type: text/html; charset=utf-8\r\n";
+		$res = mail($to, $message, $headers);
+		if($res){
+		echo 'Your Message has been sent successfully!'; 
+	} else {
+		echo 'Something went wrong, Please Try Again.'; 
+	}
+} else {
+	echo 'All Fields are Required.';
+}
