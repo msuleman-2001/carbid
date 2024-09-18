e7a00560a7027479af45434b926fcaff4284f7d6
msuleman2001 <msuleman2001@gmail.com>
2024-09-18 12:50:42 +0500

    change title of main page

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    <title>Car - Bid</title>
+    <title>Carbid</title>
 
     <link rel="stylesheet" href="/frontend/assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="/frontend/assets/css/all.min.css">
